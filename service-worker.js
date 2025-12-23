2125a167d4fd7d544f631a11279cbb0e82125978
gunterstruck <224373224+gunterstruck@users.noreply.github.com>
2025-12-23 20:17:09 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Version 1.3.0 - Historische Zitate Update
 
 const CACHE_NAME = 'kalender-cache-v1.3.0';
-const RUNTIME_CACHE = 'kalender-runtime-v1.3.1';
+const RUNTIME_CACHE = 'kalender-runtime-v1.3.2';
 
 // Dateien, die beim Install gecacht werden sollen (App Shell)
 const CACHE_URLS = [
